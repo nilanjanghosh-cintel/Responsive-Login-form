6cad2015f442f42afad6dbe0e756de10843d37b4
Nilanjan Ghosh <154585210+nilanjanghosh-cintel@users.noreply.github.com>
2024-09-24 21:51:14 +0530

    Update mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,24 +1,43 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
-.then(()=>{
-    console.log('mongoose connected');
+mongoose.connect("mongodb://localhost:27017/LoginFormPractice", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-.catch((e)=>{
-    console.log('failed');
-})
+  .then(() => {
+    console.log("Mongoose connected successfully.");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error: ", err.message);
+  });
 
-const logInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
+// Define the schema for the login collection
+const logInSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hash password before saving to the database
+logInSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (err) {
+      return next(err);
     }
-})
+  }
+  next();
+});
 
-const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+// Create the model
+const LogInCollection = mongoose.model("LogInCollection", logInSchema);
 
-module.exports=LogInCollection
+module.exports = LogInCollection;
